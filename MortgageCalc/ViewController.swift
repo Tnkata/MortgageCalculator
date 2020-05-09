d27623b8542dec5782c05332ec62389e29e4c909
Timothy Nkata <timnkata@gmail.com>
2020-05-09 17:56:33 -0400

    Simple Mortgage Calculator on the go

--- MortgageCalc/ViewController.swift
+++ MortgageCalc/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var PrincipalTextField: UITextField!
+    
+    
+    @IBOutlet weak var NumOfPaymentsTextField: UITextField!
+    
+    
+    @IBOutlet weak var InterestTextField: UITextField!
+    
+    
+    @IBOutlet weak var EstimatedMonthlyPaymentLabel: UILabel!
+    
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        button.layer.cornerRadius = 25.0
+        
     }
-
+    
+    
+    
+    @IBAction func tappedOnCalculateButton(_ sender: Any) {
+        
+        let principalText = Double(PrincipalTextField.text!)
+        
+        let numOfPaymentText = Double(NumOfPaymentsTextField.text!)
+        
+        let interestText = Double(InterestTextField.text!)
+        
+        
+        let interest = (interestText! / 100) / 12
+        
+        //print(interest) - Used to debug
+        
+        let numPayments = numOfPaymentText! * 12
+        
+        //print(numPayments) - Used to debug
+        
+        let onePlusR = 1 + interest
+        
+        let monthlyPay = (principalText!) * ((interest * pow(onePlusR, numPayments)) / ((pow(onePlusR, numPayments)) - 1))
+        
+        EstimatedMonthlyPaymentLabel.text = String(monthlyPay)
+        
+    }
+    
+    
+    
+    
+    
 
 }
 
